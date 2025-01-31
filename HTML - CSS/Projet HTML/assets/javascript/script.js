f052842dfb48157b880ebf187dc3700c42da6ccd
Youcef <35342519+youcefaddou@users.noreply.github.com>
2025-01-31 11:53:43 +0100

    Adding files and folders
    files and folders added,

---
+++ HTML - CSS/Projet HTML/assets/javascript/script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    document.getElementById('confirmation-message').style.display = 'block';
+});
+
